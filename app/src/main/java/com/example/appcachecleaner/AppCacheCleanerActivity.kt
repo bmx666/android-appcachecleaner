84482702de301ad8bb211e6aa515c464de5f82f4
Maxim Paymushkin <bmx666@users.noreply.github.com>
2022-04-02 22:51:50 +0200

    Initial application
    Signed-off-by: Maxim Paymushkin <bmx666@users.noreply.github.com>

---
+++ app/src/main/java/com/example/appcachecleaner/AppCacheCleanerActivity.kt
@@ -0,0 +1,170 @@
+package com.example.appcachecleaner
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageInfo
+import android.net.Uri
+import android.os.Bundle
+import android.os.ConditionVariable
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appcachecleaner.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
+import java.util.concurrent.atomic.AtomicBoolean
+
+class AppCacheCleanerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.btnCleanUserAppCache.setOnClickListener {
+            binding.btnOpenAccessibility.isEnabled = false
+            binding.btnCleanUserAppCache.isEnabled = false
+            binding.btnCleanSystemAppCache.isEnabled = false
+
+            CoroutineScope(IO).launch {
+                startCleanCache(getListInstalledUserApps())
+            }
+        }
+
+        binding.btnCleanSystemAppCache.setOnClickListener {
+            binding.btnOpenAccessibility.isEnabled = false
+            binding.btnCleanUserAppCache.isEnabled = false
+            binding.btnCleanSystemAppCache.isEnabled = false
+            CoroutineScope(IO).launch {
+                startCleanCache(getListInstalledSystemApps())
+            }
+        }
+
+        binding.btnOpenAccessibility.setOnClickListener {
+            // if not construct intent to request permission
+            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            // request permission via start activity for result
+            startActivity(intent)
+        }
+
+        if (checkAccessibilityPermission())
+            binding.textView.text = ""
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (!cleanCacheFinished.get()) return
+
+        if (!checkAccessibilityPermission()) {
+            Toast.makeText(this, getText(R.string.text_enable_accessibility_permission), Toast.LENGTH_SHORT).show()
+            binding.textView.text = getText(R.string.text_enable_accessibility)
+            binding.btnCleanUserAppCache.isEnabled = false
+            binding.btnCleanSystemAppCache.isEnabled = false
+        } else {
+            binding.btnCleanUserAppCache.isEnabled = true
+            binding.btnCleanSystemAppCache.isEnabled = true
+        }
+    }
+
+    private suspend fun startCleanCache(pkgInfoList: List<PackageInfo>) {
+        cleanCacheInterrupt.set(false)
+        cleanCacheFinished.set(false)
+
+        for (i in pkgInfoList.indices) {
+            val pkgName = pkgInfoList[i].packageName
+            startApplicationDetailsActivity(pkgName)
+            cleanAppCacheFinished.set(false)
+            runOnUiThread {
+                binding.textView.text = String.format(Locale.getDefault(),
+                    "%d / %d %s", i, pkgInfoList.size, getText(R.string.text_clean_cache_left))
+            }
+            delay(500L)
+            waitAccessibility.block(5000L)
+            delay(500L)
+
+            // user interrupt process
+            if (cleanCacheInterrupt.get()) break
+        }
+        cleanCacheFinished.set(true)
+
+        runOnUiThread {
+            binding.textView.text = if (cleanCacheInterrupt.get())
+                    getText(R.string.text_clean_cache_interrupt)
+                    else getText(R.string.text_clean_cache_finish)
+            binding.btnOpenAccessibility.isEnabled = true
+            binding.btnCleanUserAppCache.isEnabled = true
+            binding.btnCleanSystemAppCache.isEnabled = true
+        }
+    }
+
+    // method to check is the user has permitted the accessibility permission
+    // if not then prompt user to the system's Settings activity
+    private fun checkAccessibilityPermission(): Boolean {
+        var accessEnabled = 0
+        try {
+            accessEnabled =
+                Settings.Secure.getInt(this.contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED)
+        } catch (e: Settings.SettingNotFoundException) {
+            e.printStackTrace()
+        }
+
+        return accessEnabled != 0
+    }
+
+    private fun getListInstalledUserApps(): List<PackageInfo> {
+        val pkgInfoList = ArrayList<PackageInfo>()
+        val list = packageManager.getInstalledPackages(0)
+        for (i in list.indices) {
+            val packageInfo = list[i]
+            val flags = packageInfo!!.applicationInfo.flags
+            val isSystemApp = (flags and ApplicationInfo.FLAG_SYSTEM) != 0
+            val isUpdatedSystemApp = (flags and ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0
+            if (!isSystemApp or isUpdatedSystemApp)
+                pkgInfoList.add(packageInfo)
+        }
+        return pkgInfoList
+    }
+
+    private fun getListInstalledSystemApps(): List<PackageInfo> {
+        val pkgInfoList = ArrayList<PackageInfo>()
+        val list = packageManager.getInstalledPackages(0)
+        for (i in list.indices) {
+            val packageInfo = list[i]
+            val flags = packageInfo!!.applicationInfo.flags
+            val isSystemApp = (flags and ApplicationInfo.FLAG_SYSTEM) != 0
+            val isUpdatedSystemApp = (flags and ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0
+            if (isSystemApp and !isUpdatedSystemApp)
+                pkgInfoList.add(packageInfo)
+        }
+        return pkgInfoList
+    }
+
+    private fun startApplicationDetailsActivity(packageName: String) {
+        try {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            intent.data = Uri.parse("package:$packageName")
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            e.printStackTrace()
+        }
+    }
+
+    companion object {
+        val cleanAppCacheFinished = AtomicBoolean(false)
+        val cleanCacheFinished = AtomicBoolean(true)
+        val cleanCacheInterrupt = AtomicBoolean(false)
+        val waitAccessibility = ConditionVariable()
+        private val TAG = AppCacheCleanerActivity::class.java.simpleName
+    }
+}
---
+++ app/src/main/java/com/example/appcachecleaner/AppCacheCleanerService.kt
@@ -0,0 +1,171 @@
+package com.example.appcachecleaner
+
+import android.accessibilityservice.AccessibilityButtonController
+import android.accessibilityservice.AccessibilityButtonController.AccessibilityButtonCallback
+import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.os.Build
+import android.util.Log
+import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
+import androidx.annotation.RequiresApi
+
+class AppCacheCleanerService : AccessibilityService() {
+
+    private var mAccessibilityButtonController: AccessibilityButtonController? = null
+    private var mAccessibilityButtonCallback: AccessibilityButtonCallback? = null
+    private var mIsAccessibilityButtonAvailable: Boolean = false
+
+    private fun compareText(nodeInfo: AccessibilityNodeInfo, text: CharSequence): Boolean {
+        //Log.v(TAG, "search string $text")
+        return nodeInfo.text?.toString()?.lowercase().contentEquals(text.toString().lowercase())
+    }
+
+    private fun findClearCacheButton(nodeInfo: AccessibilityNodeInfo): AccessibilityNodeInfo? {
+        for (i in 0 until nodeInfo.childCount) {
+            if (nodeInfo.getChild(i) == null) continue
+            val found = findClearCacheButton(nodeInfo.getChild(i))
+            if (found != null) return found
+        }
+
+        return if (
+            nodeInfo.viewIdResourceName?.startsWith("com.android.settings:id/button") == true
+            && compareText(nodeInfo, getText(R.string.clear_cache_btn_text))
+        ) nodeInfo else null
+    }
+
+    private fun findStorageAndCacheMenu(nodeInfo: AccessibilityNodeInfo): AccessibilityNodeInfo? {
+        for (i in 0 until nodeInfo.childCount) {
+            if (nodeInfo.getChild(i) == null) continue
+            val found = findStorageAndCacheMenu(nodeInfo.getChild(i))
+            if (found != null) return found
+        }
+
+        return if (
+            nodeInfo.viewIdResourceName?.contentEquals("android:id/title") == true
+            && compareText(nodeInfo, getText(R.string.storage_settings_for_app))
+        ) nodeInfo.parent?.parent else null
+    }
+
+    private fun findBackButton(nodeInfo: AccessibilityNodeInfo): AccessibilityNodeInfo? {
+        val actionBar = nodeInfo.findAccessibilityNodeInfosByViewId(
+            "com.android.settings:id/action_bar").firstOrNull() ?: return null
+
+        for (i in 0 until actionBar.childCount) {
+            if (actionBar.getChild(i)?.className?.contentEquals("android.widget.ImageButton") == true)
+                return actionBar.getChild(i)
+        }
+        return null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mAccessibilityButtonCallback?.let {
+                mAccessibilityButtonController?.unregisterAccessibilityButtonCallback(it)
+            }
+        }
+    }
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            initButton()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun initButton() {
+        mAccessibilityButtonController = accessibilityButtonController
+        mIsAccessibilityButtonAvailable = true
+            //mAccessibilityButtonController?.isAccessibilityButtonAvailable ?: false
+
+        if (!mIsAccessibilityButtonAvailable) return
+
+        serviceInfo = serviceInfo.apply {
+            flags = flags or AccessibilityServiceInfo.FLAG_REQUEST_ACCESSIBILITY_BUTTON
+        }
+
+        mAccessibilityButtonCallback =
+            object : AccessibilityButtonCallback() {
+                override fun onClicked(controller: AccessibilityButtonController) {
+                    if (AppCacheCleanerActivity.cleanCacheFinished.get()) return
+                    Log.d(TAG, "Accessibility button pressed!")
+                    AppCacheCleanerActivity.cleanCacheInterrupt.set(true)
+                    AppCacheCleanerActivity.waitAccessibility.open()
+                }
+
+                override fun onAvailabilityChanged(
+                    controller: AccessibilityButtonController,
+                    available: Boolean
+                ) {
+                    if (controller == mAccessibilityButtonController) {
+                        Log.d(TAG, "Accessibility button available = $available")
+                        mIsAccessibilityButtonAvailable = available
+                    }
+                }
+            }
+
+        mAccessibilityButtonCallback?.also {
+            mAccessibilityButtonController?.registerAccessibilityButtonCallback(it)
+        }
+    }
+
+    private fun showTree(level: Int, nodeInfo: AccessibilityNodeInfo) {
+        Log.v(TAG, ">".repeat(level) + " " + nodeInfo.className
+                + ":" + nodeInfo.text+ ":" + nodeInfo.viewIdResourceName)
+        for (i in 0 until nodeInfo.childCount)
+            showTree(level + 1, nodeInfo.getChild(i))
+    }
+
+    private fun goBack(nodeInfo: AccessibilityNodeInfo) {
+        findBackButton(nodeInfo)?.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+        AppCacheCleanerActivity.cleanAppCacheFinished.set(true)
+    }
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent) {
+        if (AppCacheCleanerActivity.cleanCacheFinished.get()) return
+
+        if (event.eventType != AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED)
+            return
+
+        val nodeInfo = event.source ?: return
+        // showTree(0, nodeInfo)
+
+        if (AppCacheCleanerActivity.cleanAppCacheFinished.get()) {
+            goBack(nodeInfo)
+            // notify main app go to another app
+            AppCacheCleanerActivity.waitAccessibility.open()
+        } else {
+            val storageAndCacheMenu = findStorageAndCacheMenu(nodeInfo)
+            val clearCacheButton = findClearCacheButton(nodeInfo)
+
+            if (storageAndCacheMenu != null) {
+                if (storageAndCacheMenu.isEnabled) {
+                    //Log.v(TAG, "found and click storage & cache")
+                    storageAndCacheMenu.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+                } else {
+                    goBack(nodeInfo)
+                    // notify main app go to another app
+                    AppCacheCleanerActivity.waitAccessibility.open()
+                }
+            } else if (clearCacheButton != null) {
+                if (clearCacheButton.isEnabled) {
+                    //Log.v(TAG, "found and click clean cache button")
+                    clearCacheButton.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+                }
+                goBack(nodeInfo)
+            } else {
+                goBack(nodeInfo)
+                // notify main app go to another app
+                AppCacheCleanerActivity.waitAccessibility.open()
+            }
+        }
+    }
+
+    override fun onInterrupt() {}
+
+    companion object {
+        private val TAG = AppCacheCleanerService::class.java.simpleName
+    }
+}
