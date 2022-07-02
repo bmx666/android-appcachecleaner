9178499e410072f6e7ecb3aaffe0bcea8dbcd9ec
Maxim Paymushkin <bmx666@users.noreply.github.com>
2022-07-02 15:54:42 +0200

    Timber: prepend priority for log file

--- app/src/main/java/com/github/bmx666/appcachecleaner/log/TimberFileTree.kt
+++ app/src/main/java/com/github/bmx666/appcachecleaner/log/TimberFileTree.kt
@@ -1,11 +1,20 @@
 package com.github.bmx666.appcachecleaner.log
 
+import android.util.Log
 import timber.log.Timber
 import java.io.File
 
 
 class TimberFileTree(private val log: File) : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        when (priority) {
+            Log.VERBOSE -> log.appendText("V: ")
+            Log.DEBUG   -> log.appendText("D: ")
+            Log.INFO    -> log.appendText("I: ")
+            Log.WARN    -> log.appendText("W: ")
+            Log.ERROR   -> log.appendText("E: ")
+            Log.ASSERT  -> log.appendText("A: ")
+        }
         log.appendText(message + "\n")
     }
 }
