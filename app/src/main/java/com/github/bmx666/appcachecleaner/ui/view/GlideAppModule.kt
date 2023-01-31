8fede5e21e8f31c4bb7d3bc42399a0a47df17d0a
Maxim Paymushkin <bmx666@users.noreply.github.com>
2023-01-31 21:53:44 +0100

    add GeneratedAppGlideModule
    fixes #63

---
+++ app/src/main/java/com/github/bmx666/appcachecleaner/ui/view/GlideAppModule.kt
@@ -0,0 +1,7 @@
+package com.github.bmx666.appcachecleaner.ui.view
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideAppModule: AppGlideModule()
