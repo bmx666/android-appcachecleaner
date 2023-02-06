ae6c64f20a58900f93c7c216a720d557e528c266
Maxim Paymushkin <bmx666@users.noreply.github.com>
2023-02-06 23:21:28 +0100

    fix pass context for AccessibilityOverlay

--- app/src/main/java/com/github/bmx666/appcachecleaner/ui/view/AccessibilityOverlay.kt
+++ app/src/main/java/com/github/bmx666/appcachecleaner/ui/view/AccessibilityOverlay.kt
@@ -9,22 +9,21 @@ import android.widget.ImageButton
 import com.github.bmx666.appcachecleaner.R
 
 class AccessibilityOverlay(
-    private val context: Context,
     private val callback: () -> Unit
 ) {
 
-    private val button: ImageButton = LayoutInflater.from(context)
-        .inflate(R.layout.accessibility_overlay_button, null) as ImageButton
+    private var imageButton: ImageButton? = null
 
-    init {
-        button.setOnClickListener { callback.invoke() }
+    private fun getImageButton(context: Context): ImageButton? {
+        return LayoutInflater.from(context).inflate(R.layout.accessibility_overlay_button, null)
+                as ImageButton?
     }
 
-    private fun getWindowManager(): WindowManager {
-        return context.getSystemService(AccessibilityService.WINDOW_SERVICE) as WindowManager
+    private fun getWindowManager(context: Context): WindowManager? {
+        return context.getSystemService(AccessibilityService.WINDOW_SERVICE) as WindowManager?
     }
 
-    fun show() {
+    fun show(context: Context) {
         val params = WindowManager.LayoutParams().apply {
             height = WindowManager.LayoutParams.WRAP_CONTENT
             width = WindowManager.LayoutParams.WRAP_CONTENT
@@ -35,10 +34,27 @@ class AccessibilityOverlay(
                     WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
         }
 
-        getWindowManager().addView(button, params)
+        hide(context)
+        try {
+            getImageButton(context)?.let { button ->
+                button.setOnClickListener { callback.invoke() }
+                imageButton = button
+                getWindowManager(context)?.addView(imageButton, params)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            imageButton = null
+        }
     }
 
-    fun hide() {
-        getWindowManager().removeView(button)
+    fun hide(context: Context) {
+        imageButton ?: return
+        try {
+            getWindowManager(context)?.removeView(imageButton)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            imageButton = null
+        }
     }
 }
