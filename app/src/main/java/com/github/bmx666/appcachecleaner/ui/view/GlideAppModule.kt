c178fbb369b8c04c6d1a3871dab1317415e3a3e5
Maxim Paymushkin <bmx666@users.noreply.github.com>
2023-11-08 23:09:29 +0100

    build.gradle: update dependencies, migrate on ksp

--- app/src/main/java/com/github/bmx666/appcachecleaner/ui/view/GlideAppModule.kt
+++
@@ -1,7 +0,0 @@
-package com.github.bmx666.appcachecleaner.ui.view
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class GlideAppModule: AppGlideModule()
