6592194f641bd4d04b2ef85831bdeae67363bbf1
Maxim Paymushkin <bmx666@users.noreply.github.com>
2023-11-11 00:46:12 +0100

    skip first boot in debug mode

--- app/src/main/java/com/github/bmx666/appcachecleaner/ui/activity/LauncherActivity.kt
+++ app/src/main/java/com/github/bmx666/appcachecleaner/ui/activity/LauncherActivity.kt
@@ -3,21 +3,25 @@ package com.github.bmx666.appcachecleaner.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.bmx666.appcachecleaner.BuildConfig
 import com.github.bmx666.appcachecleaner.config.SharedPreferencesManager
 
 class LauncherActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (SharedPreferencesManager.FirstBoot.showFirstBootConfirmation(this)) {
-            // It's the first boot, show FirstBootActivity
-            val intent = Intent(this, FirstBootActivity::class.java)
-            startActivity(intent)
-        } else {
-            // Not the first boot, show AppCacheCleanerActivity
-            val intent = Intent(this, AppCacheCleanerActivity::class.java)
-            startActivity(intent)
-        }
+        startActivity(
+            Intent(this,
+                // It's the first boot, show FirstBootActivity
+                if (!BuildConfig.DEBUG
+                    && SharedPreferencesManager
+                        .FirstBoot
+                        .showFirstBootConfirmation(this))
+                    FirstBootActivity::class.java
+                // Not the first boot, show AppCacheCleanerActivity
+                else AppCacheCleanerActivity::class.java
+            )
+        )
 
         // Close this activity so it's not in the back stack
         finishAfterTransition()
