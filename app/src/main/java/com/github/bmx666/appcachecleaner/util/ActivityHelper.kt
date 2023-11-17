cee20c0d7f5378d4a437ba2ba760c8347940606a
Maxim Paymushkin <bmx666@users.noreply.github.com>
2023-11-17 19:31:10 +0100

    ActivityHelper: refactor
    Check if context is Service and add flag
    FLAG_ACTIVITY_NEW_TASK
    
    Use Context? argument instead of Context to avoid NPE

--- app/src/main/java/com/github/bmx666/appcachecleaner/util/ActivityHelper.kt
+++ app/src/main/java/com/github/bmx666/appcachecleaner/util/ActivityHelper.kt
@@ -1,11 +1,11 @@
 package com.github.bmx666.appcachecleaner.util
 
+import android.app.Service
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
-import com.github.bmx666.appcachecleaner.service.CacheCleanerTileService
 
 class ActivityHelper {
 
@@ -21,7 +21,7 @@ class ActivityHelper {
                         addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
                         addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
                         addFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION)
-                        if (context is CacheCleanerTileService)
+                        if (context is Service)
                             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                         data = Uri.parse("package:$packageName")
                     }
@@ -32,26 +32,30 @@ class ActivityHelper {
         }
 
         @JvmStatic
-        fun showAccessibilitySettings(context: Context) {
-            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-            if (context is CacheCleanerTileService)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context.startActivity(intent)
+        fun showAccessibilitySettings(context: Context?) {
+            context?.startActivity(
+                Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                    if (context is Service)
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+            )
         }
 
         @JvmStatic
-        fun showUsageAccessSettings(context: Context) {
-            val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-            if (context is CacheCleanerTileService)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context.startActivity(intent)
+        fun showUsageAccessSettings(context: Context?) {
+            context?.startActivity(
+                Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                    if (context is Service)
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+            )
         }
 
         @JvmStatic
-        fun returnBackToMainActivity(context: Context, intent: Intent) {
-            context.startActivity(
+        fun returnBackToMainActivity(context: Context?, intent: Intent) {
+            context?.startActivity(
                 intent.apply {
                     addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
                     addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
@@ -63,12 +67,13 @@ class ActivityHelper {
         }
 
         @JvmStatic
-        fun navigateToHomeScreen(context: Context) {
-            context.startActivity(
+        fun navigateToHomeScreen(context: Context?) {
+            context?.startActivity(
                 Intent(Intent.ACTION_MAIN).apply {
-                addCategory(Intent.CATEGORY_HOME)
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            })
+                    addCategory(Intent.CATEGORY_HOME)
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+            )
         }
     }
 }
