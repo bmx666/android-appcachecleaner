ac3001126c63a5a0cf3d9b89743bb541c7970f52
Maxim Paymushkin <bmx666@users.noreply.github.com>
2023-12-11 21:57:27 +0100

    check darkTheme from Settings

--- app/src/main/java/com/github/bmx666/appcachecleaner/ui/theme/Theme.kt
+++ app/src/main/java/com/github/bmx666/appcachecleaner/ui/theme/Theme.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.github.bmx666.appcachecleaner.config.SharedPreferencesManager
 
 
 private val LightColorScheme = lightColorScheme(
@@ -83,14 +84,15 @@ private val DarkColorScheme = darkColorScheme(
 
 @Composable
 fun AppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val context = LocalContext.current
+    val darkTheme = isSystemInDarkTheme() || SharedPreferencesManager.UI.getNightMode(context)
+
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
