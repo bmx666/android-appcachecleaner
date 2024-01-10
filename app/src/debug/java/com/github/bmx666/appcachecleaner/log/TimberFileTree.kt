92b735c8b59e35fd6404cda069a7f4a7f55a377b
Maxim Paymushkin <bmx666@users.noreply.github.com>
2024-01-10 21:07:28 +0100

    TimberFileTree: fix race condition to save into file

--- app/src/debug/java/com/github/bmx666/appcachecleaner/log/TimberFileTree.kt
+++ app/src/debug/java/com/github/bmx666/appcachecleaner/log/TimberFileTree.kt
@@ -8,13 +8,12 @@ import java.io.File
 class TimberFileTree(private val log: File) : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         when (priority) {
-            Log.VERBOSE -> log.appendText("V: ")
-            Log.DEBUG   -> log.appendText("D: ")
-            Log.INFO    -> log.appendText("I: ")
-            Log.WARN    -> log.appendText("W: ")
-            Log.ERROR   -> log.appendText("E: ")
-            Log.ASSERT  -> log.appendText("A: ")
+            Log.VERBOSE -> log.appendText("V: $message\n")
+            Log.DEBUG   -> log.appendText("D: $message\n")
+            Log.INFO    -> log.appendText("I: $message\n")
+            Log.WARN    -> log.appendText("W: $message\n")
+            Log.ERROR   -> log.appendText("E: $message\n")
+            Log.ASSERT  -> log.appendText("A: $message\n")
         }
-        log.appendText(message + "\n")
     }
 }
