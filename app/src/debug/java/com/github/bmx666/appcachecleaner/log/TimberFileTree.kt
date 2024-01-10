1b75d01c561c34b0ceeeabc28902ea3ab909e46c
Maxim Paymushkin <bmx666@users.noreply.github.com>
2024-01-10 21:17:56 +0100

    TimberFileTree: add timestamp

--- app/src/debug/java/com/github/bmx666/appcachecleaner/log/TimberFileTree.kt
+++ app/src/debug/java/com/github/bmx666/appcachecleaner/log/TimberFileTree.kt
@@ -3,17 +3,21 @@ package com.github.bmx666.appcachecleaner.log
 import android.util.Log
 import timber.log.Timber
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 
 class TimberFileTree(private val log: File) : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        val sdf = SimpleDateFormat("HH:mm:ss.SSS", Locale.US)
+        val formattedTime = sdf.format(System.currentTimeMillis())
         when (priority) {
-            Log.VERBOSE -> log.appendText("V: $message\n")
-            Log.DEBUG   -> log.appendText("D: $message\n")
-            Log.INFO    -> log.appendText("I: $message\n")
-            Log.WARN    -> log.appendText("W: $message\n")
-            Log.ERROR   -> log.appendText("E: $message\n")
-            Log.ASSERT  -> log.appendText("A: $message\n")
+            Log.VERBOSE -> log.appendText("[$formattedTime] V: $message\n")
+            Log.DEBUG   -> log.appendText("[$formattedTime] D: $message\n")
+            Log.INFO    -> log.appendText("[$formattedTime] I: $message\n")
+            Log.WARN    -> log.appendText("[$formattedTime] W: $message\n")
+            Log.ERROR   -> log.appendText("[$formattedTime] E: $message\n")
+            Log.ASSERT  -> log.appendText("[$formattedTime] A: $message\n")
         }
     }
 }
