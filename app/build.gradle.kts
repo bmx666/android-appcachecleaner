845fe84e79ae0ba30b62c16b1236430639bba5df
Maxim Paymushkin <bmx666@users.noreply.github.com>
2024-05-15 22:50:40 +0200

    build.gradle.kts: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,12 +60,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.annotation:annotation:1.7.1")
+    implementation("androidx.annotation:annotation:1.8.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.github.bumptech.glide:glide:4.14.2")
