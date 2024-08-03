c2c6e1adcd1068f558992087221b91a11171c441
Maxim Paymushkin <bmx666@users.noreply.github.com>
2024-08-03 23:59:26 +0200

    update to v1.12.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.github.bmx666.appcachecleaner"
         minSdk = 23
         targetSdk = 35
-        versionCode = 88
-        versionName = "1.12.0"
+        versionCode = 89
+        versionName = "1.12.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
