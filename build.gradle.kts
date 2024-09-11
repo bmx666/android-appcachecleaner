3ca239cb727300d7d2dfffa9c64e4b295ba898ce
Maxim Paymushkin <bmx666@users.noreply.github.com>
2024-09-11 22:46:31 +0200

    update Android Gradle Plugin 8.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("com.android.library") version "8.5.2" apply false
+    id("com.android.application") version "8.6.0" apply false
+    id("com.android.library") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
 }
