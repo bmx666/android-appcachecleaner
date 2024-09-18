6389331b64f18e839a2c8cb66400aa9b75d47c05
Maxim Paymushkin <bmx666@users.noreply.github.com>
2024-09-18 15:00:35 +0200

    update to v2.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.github.bmx666.appcachecleaner"
         minSdk = 23
         targetSdk = 35
-        versionCode = 101
-        versionName = "2.2.3"
+        versionCode = 102
+        versionName = "2.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
