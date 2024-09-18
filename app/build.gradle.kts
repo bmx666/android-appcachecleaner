6d47fa19eb24b58b71d1df9985529f47554f3f14
Maxim Paymushkin <bmx666@users.noreply.github.com>
2024-09-18 15:35:06 +0200

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,28 +86,28 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     // Compose UI
-    val composeBom = platform("androidx.compose:compose-bom:2024.03.00")
+    val composeBom = platform("androidx.compose:compose-bom:2024.09.01")
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.3.0")
 
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.1")
 
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.1")
 
-    implementation("androidx.compose.material:material-icons-core")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.compose.material3:material3-window-size-class")
+    implementation("androidx.compose.material:material-icons-core:1.7.1")
+    implementation("androidx.compose.material:material-icons-extended:1.7.1")
+    implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
 
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
     implementation("androidx.navigation:navigation-compose:2.8.0")
-    implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("androidx.compose.runtime:runtime-rxjava2")
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.1")
+    implementation("androidx.compose.runtime:runtime-rxjava2:1.7.1")
 
     implementation("androidx.datastore:datastore-core:1.1.1")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
@@ -123,5 +123,5 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     implementation("io.coil-kt:coil-compose:2.7.0")
-    implementation("com.google.accompanist:accompanist-permissions:0.34.0")
+    implementation("com.google.accompanist:accompanist-permissions:0.36.0")
 }
