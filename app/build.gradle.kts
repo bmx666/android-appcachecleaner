8cd34a5c84b9a8135678d58b9e0fda655995263d
Maxim Paymushkin <bmx666@users.noreply.github.com>
2024-10-06 17:51:44 +0200

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,22 +92,22 @@ dependencies {
 
     implementation("androidx.compose.material3:material3:1.3.0")
 
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.3")
 
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.3")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.3")
 
-    implementation("androidx.compose.material:material-icons-core:1.7.1")
-    implementation("androidx.compose.material:material-icons-extended:1.7.1")
+    implementation("androidx.compose.material:material-icons-core:1.7.3")
+    implementation("androidx.compose.material:material-icons-extended:1.7.3")
     implementation("androidx.compose.material3:material3-window-size-class:1.3.0")
 
     implementation("androidx.activity:activity-compose:1.9.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
-    implementation("androidx.navigation:navigation-compose:2.8.0")
-    implementation("androidx.compose.runtime:runtime-livedata:1.7.1")
-    implementation("androidx.compose.runtime:runtime-rxjava2:1.7.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    implementation("androidx.navigation:navigation-compose:2.8.2")
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.3")
+    implementation("androidx.compose.runtime:runtime-rxjava2:1.7.3")
 
     implementation("androidx.datastore:datastore-core:1.1.1")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
