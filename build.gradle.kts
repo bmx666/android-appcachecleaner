8b5af3d4eabf8f8f91879823ad2f5787366a042e
Maxim Paymushkin <bmx666@users.noreply.github.com>
2024-11-25 03:01:55 +0100

    update Android Gradle Plugin 8.7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("com.android.library") version "8.6.1" apply false
+    id("com.android.application") version "8.7.2" apply false
+    id("com.android.library") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
     id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
