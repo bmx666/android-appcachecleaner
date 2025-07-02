b31c94dd1bf031de6e5390525c66ca5799dbc59b
Maxim Paymushkin <bmx666@users.noreply.github.com>
2025-07-02 18:38:04 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("com.android.application") version "8.11.0" apply false
     id("com.android.library") version "8.11.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
 }
 buildscript {
     dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.53")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.56.2")
     }
 }
