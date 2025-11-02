1c81e1d880167af498131b5fe41591e960fa4a6c
Maxim Paymushkin <bmx666@users.noreply.github.com>
2025-11-02 23:46:39 +0100

    update hilt warnings

--- app/src/main/java/com/github/bmx666/appcachecleaner/ui/viewmodel/SettingsFilterViewModel.kt
+++ app/src/main/java/com/github/bmx666/appcachecleaner/ui/viewmodel/SettingsFilterViewModel.kt
@@ -21,7 +21,7 @@ import javax.inject.Inject
 @HiltViewModel
 class SettingsFilterViewModel @Inject constructor(
     private val userPrefFilterManager: UserPrefFilterManager,
-    @ApplicationContext private val context: Context
+    @param:ApplicationContext private val context: Context
 
 ) : ViewModel() {
 
