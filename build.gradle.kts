3aa12230f5e4197601877a12257045a69dca7a3e
Maxim Paymushkin <bmx666@users.noreply.github.com>
2025-11-02 23:23:50 +0100

    add dependency analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,13 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
+    id("com.autonomousapps.dependency-analysis") version "3.4.1"
 }
+
+allprojects {
+    apply(plugin = "com.autonomousapps.dependency-analysis")
+}
+
 buildscript {
     dependencies {
         classpath(libs.hilt.gradle.plugin)
